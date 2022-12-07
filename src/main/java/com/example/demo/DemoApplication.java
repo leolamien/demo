36b3ffda9c618d7bfc1aca44a343a8cc126a06c5
leolamien <90797794+leolamien@users.noreply.github.com>
2022-12-07 14:46:05 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+  @GetMapping("/message")
+  public String message(){
+    return "yessir";
+  }
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
